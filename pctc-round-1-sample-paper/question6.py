eb80b5cd6e31b1d79e0dc041adeb9da6b0bf34d0
uaerious <84714801+uaerious@users.noreply.github.com>
2023-03-03 12:39:45 +0800

    Create question6.py

---
+++ pctc-round-1-sample-paper/question6.py
@@ -0,0 +1,16 @@
+name_1 = input()
+name_2 = input()
+name_3 = input()
+name_4 = input()
+name_5 = input()
+
+if name_1 == "Ellie":
+  print("1st")
+elif name_2 == "Ellie":
+  print("2nd")
+elif name_3 == "Ellie":
+  print("3rd")
+elif name_4 == "Ellie":
+  print("4th")  
+else:
+  print("5th")
